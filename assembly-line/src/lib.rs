36448ec894d902a75e49a4173bc7870f7126f6cf
tarekcampossaleh <tarekcampossaleh@gmail.com>
2021-10-12 18:40:51 -0300

    f64 usage fix

--- assembly-line/src/lib.rs
+++ assembly-line/src/lib.rs
@@ -1,7 +1,7 @@
 #![allow(unused)]
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    speed as f64 * 221 as f64 * success_rate(speed)
+    f64::from(speed) * 221.0 * success_rate(speed)
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
