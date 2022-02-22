a6deb760bfa32d72469a22f7e7be19df8411d6b8
tarekcampossaleh <tarekcampossaleh@gmail.com>
2022-02-22 18:57:41 -0300

    feat(role-playing-game): solution

--- role-playing-game/src/lib.rs
+++ role-playing-game/src/lib.rs
@@ -1,6 +1,7 @@
 // This stub file contains items which aren't used yet; feel free to remove this module attribute
 // to enable stricter warnings.
 #![allow(unused)]
+use std::cmp::min;
 
 pub struct Player {
     pub health: u32,
@@ -9,7 +10,31 @@ pub struct Player {
 }
 
 impl Player {
-    pub fn revive(&self) -> Option<Player> {}
+    pub fn revive(&self) -> Option<Player> {
+        match self.health {
+            0 => Some(Player {
+                health: 100,
+                mana: if self.level < 10 { None } else { Some(100) },
+                level: self.level,
+            }),
+            _ => None,
+        }
+    }
 
-    pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {}
+    pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
+        match self.mana {
+            Some(x) => {
+                if x >= mana_cost {
+                    self.mana = Some(x - mana_cost);
+                    mana_cost * 2
+                } else {
+                    0
+                }
+            }
+            None => {
+                self.health -= min(self.health, mana_cost);
+                0
+            }
+        }
+    }
 }
